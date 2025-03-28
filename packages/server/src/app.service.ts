13c3dc46bcd1ca600219eab7ff7cfe6c15c366ae
CornWorld <github.contact@corn.im>
2025-03-28 21:15:45 +0800

    [Chore] Replace `lint-staged` with `nano-staged`
    https://github.com/CornWorld/vanblog/issues/4

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!\n Visit https://github.com/CornWorld/vanblog for more information, like API Docs, etc.';
   }
 }
