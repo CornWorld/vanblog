6c9b076f3fcfc170c73a34c66b80277bf31114df
CornWorld <github.contact@corn.im>
2025-04-20 01:26:44 +0800

    [Chore] Add `next-i18next`

---
+++ packages/website/next-i18next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next-i18next').UserConfig} */
+const config = {
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['zh'],
+  },
+  defaultNS: 'translations',
+  ns: ['translations'],
+};
+
+export default config;
