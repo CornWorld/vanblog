1d9500dfc0c3dcf4222d0ac66fd392bec7beae36
JamesGosing <3248175240@qq.com>
2025-05-24 16:13:31 +0800

    perf: 1.优化统一响应处理类;2.更新响应枚举值

--- packages/server-new/src/common/result/ResultCodeEnum.ts
+++ packages/server-new/src/common/result/ResultCodeEnum.ts
@@ -1,17 +1,23 @@
-// 定义结果代码枚举
+/**
+ * @description 结果代码枚举定义
+ */
 export enum ResultCodeEnum {
   SUCCESS = 200,
-  FAIL = 201,
-  LOGIN_ERROR = 205,
-  LOGIN_AUTH = 208,
-  PERMISSION = 209,
+  FAIL = 500,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
 }
 
-// 获取枚举对应的消息
+/**
+ * @description 枚举对应的消息
+ */
 export const ResultCodeMessage: Record<ResultCodeEnum, string> = {
   [ResultCodeEnum.SUCCESS]: '成功',
-  [ResultCodeEnum.FAIL]: '失败',
-  [ResultCodeEnum.LOGIN_ERROR]: '认证失败',
-  [ResultCodeEnum.LOGIN_AUTH]: '未登陆',
-  [ResultCodeEnum.PERMISSION]: '没有权限',
+  [ResultCodeEnum.FAIL]: '服务器异常',
+  [ResultCodeEnum.BAD_REQUEST]: '请求参数异常',
+  [ResultCodeEnum.UNAUTHORIZED]: '认证失败',
+  [ResultCodeEnum.FORBIDDEN]: '权限不足',
+  [ResultCodeEnum.NOT_FOUND]: '没有找到该资源',
 };
