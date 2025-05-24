0e43677f31ae065683093fed8b37d65feff447c0
LittleStrange <1934944134@qq.com>
2025-05-24 16:04:39 +0800

    perf: use result builder instead of json

--- packages/server-new/src/common/exception/http-exception.filter.ts
+++ packages/server-new/src/common/exception/http-exception.filter.ts
@@ -1,6 +1,7 @@
 import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
 import { Response } from 'express';
 import { ResultCodeEnum } from '../result/ResultCodeEnum';
+import { Result } from '../result/Result';
 
 /**
  * 捕获未被捕获的运行时异常
@@ -12,9 +13,6 @@ export class ErrorFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const status = ResultCodeEnum.FAIL;
     console.error("发生未被捕获异常：" + exception.message); // 获取请求信息
-    response.status(status).json({
-      statusCode: status,
-      message: '发生未知内部异常',
-    });
+    response.status(status).json(Result.build(status, "发生未知内部异常"));
   }
 }
