26e567960868189615809a55f4a15902a1af2bad
LittleStrange <1934944134@qq.com>
2025-05-25 15:33:56 +0800

    chore: migrate backup module

---
+++ packages/server-new/src/service/backup/backup.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { BackupController } from './controller/backup.controller';
+
+@Module({
+  imports: [],
+  controllers: [
+    BackupController
+  ],
+  providers: [],
+})
+export class BackupModule { }
