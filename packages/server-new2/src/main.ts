e900c5d2473886956f4e0f6cfae5d43cb7342ec7
CornWorld <cw@had.ink>
2025-06-12 01:12:13 +0800

    feat: use vite and vitest in server-ng

--- packages/server-new2/src/main.ts
+++ packages/server-new2/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+// @ts-expect-error import.meta.env is not available in the test environment
+if (import.meta.env.PROD) {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }
+
+  bootstrap();
 }
-bootstrap();
+
+export const viteNodeApp = NestFactory.create(AppModule);
---
+++ packages/server-new2/vitest.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+import swc from 'unplugin-swc';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    root: './',
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+  },
+  resolve: {
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+});
